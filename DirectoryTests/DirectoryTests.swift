852df731a06c066e17837f7ea5eb69ec3268aa5d
Harold Martin <harold.martin@gmail.com>
2020-01-09 14:07:00 -0800

    Initial Commit

---
+++ DirectoryTests/DirectoryTests.swift
@@ -0,0 +1,34 @@
+//
+//  DirectoryTests.swift
+//  DirectoryTests
+//
+//  Created by Harold Martin on 1/9/20.
+//  Copyright © 2020 Harold Martin. All rights reserved.
+//
+
+import XCTest
+@testable import Directory
+
+class DirectoryTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
